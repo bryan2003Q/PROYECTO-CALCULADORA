59fcc92d7ecce4d22ce09936bcdb278c8299a6ea
BryanQ <quishpebryan1234@gmail.com>
2025-11-26 09:51:27 -0500

    agregar funciones de resta, multiplicación y división en app.py

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 def sumar(a, b):
     return a + b
 
+def restar (a, b):
+    return a - b
+
+def multiplicar (a, b):
+    return a * b
+
+def dividir (a, b):
+    if b == 0:
+        return "Error: No se puede dividir para cero"
+    return a/b 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     resultado = None
@@ -12,7 +23,21 @@ def home():
         try:
             val1 = float(request.form['val1'])
             val2 = float(request.form['val2'])
-            resultado = sumar(val1, val2)
+            operacion = request.form.get('op')
+            
+            if operacion == "multiplicar":
+                resultado = multiplicar(val1, val2)
+
+            elif operacion == "restar":
+                resultado = restar(val1, val2)
+
+            elif operacion == "dividir":
+                resultado = dividir(val1, val2)
+
+            else:  # sumar por defecto
+                resultado = sumar(val1, val2)
+            
+            
         except ValueError:
             resultado = "Error: Ingresa números válidos"
     return render_template('index.html', resultado=resultado)
--- tests.py
+++ tests.py
@@ -1,8 +1,13 @@
 import pytest
 from app import sumar
+from app import multiplicar
 
 def test_suma_positivos():
     assert sumar(10, 20) == 30
 
 def test_suma_negativos():
     assert sumar(-5, -5) == -10
+    
+
+    
+    
